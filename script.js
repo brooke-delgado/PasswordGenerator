78bfe93dc7ca1626b37743ab6c66b315bd28f443
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-08-10 20:17:39 -0500

    password length

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  password = "";
+  passwordCharacters = "";
+
+  passwordlength = prompt("Password Must Be Between 8 and 128 Characters")
+ 
+}
+
+if(passwordlength<=8 || passwordlength<=128) {
+ //if password does not satisfy the 8 charcter length then Alert- "Password length must be at least 8 characters." Return- back//
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
