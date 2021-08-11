1ab600616dc462d15ab3fb401b89c857e7190ddc
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-08-10 20:52:31 -0500

    added prompts

--- script.js
+++ script.js
@@ -18,11 +18,20 @@ function generatePassword() {
  
 }
 
-if(passwordlength<=8 || passwordlength<=128) {
+if(passwordlength>=8 || passwordlength<=128) {
  //if password does not satisfy the 8 charcter length then Alert- "Password length must be at least 8 characters." Return- back//
 
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Prompts that pop up after Generate Password button is clicked//
+
+function generatePassword() {
+  var passwordlength = prompt ("Please enter number of characters you want your new password to be. It must be between 8 and 128 characters in length")
+  var upperCases = confirm("Would you like to use uppercases in your password?");
+  var lowerCases = confirm("Would you like to use lowercases in your password?");
+  var numbers = confirm("would you like to use numers in your password?");
+  var characters = confirm ("Would you like to use characters in your password?");
+}
